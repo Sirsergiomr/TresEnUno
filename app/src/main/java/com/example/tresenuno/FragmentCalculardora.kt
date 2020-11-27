34103ceeaa877d4a31d3bca9e33bf70f8f73f87c
Sirsergiomr <sirsergio.mr@gmail.com>
2020-11-27 12:32:16 +0100

    Calculadora terminada , falta la cámara

--- app/src/main/java/com/example/tresenuno/FragmentCalculardora.kt
+++ app/src/main/java/com/example/tresenuno/FragmentCalculardora.kt
@@ -33,135 +33,158 @@ class FragmentCalculardora : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculardora, container, false)
     }
-    fun numbers(v: View) {
-        val num = (v.id)
-        textView.text = textView.text.toString() + num.toString()
-    }
 
-    fun punto(v: View) {
-        var entrada = textView.getText().toString()
-        var bt13 = R.id.button13
-        if (entrada == "") {
-            Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
-        } else if (entrada.contains('.')) {
-            Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
-            button13.isEnabled = false
-        } else {
-            textView.text = textView.text.toString() + "."
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+
         }
-    }
-
-
-
-    fun btSuma(v: View) {
-        button13.isEnabled = true
-        if (textView.getText().toString() == "") {
-            Toast.makeText(context, "No has puesto nada", Toast.LENGTH_LONG).show()
-        } else {
-            num1 = textView.getText().toString().toDouble()
-            operator = "+"
-            textView.text = ""
+        bt0.setOnClickListener() {
+            textView.text = textView.text.toString() + 0.toString()
         }
-    }
-
-    fun btResta(v: View) {
-        button13.isEnabled = true
-        if (textView.getText().toString() == "") {
-            textView.text = "-"
-        } else if (comprueba() == false) {
-            num1 = textView.getText().toString().toDouble()
-            operator = "-"
-            textView.text = ""
+        bt1.setOnClickListener() {
+            textView.text = textView.text.toString() + 1.toString()
         }
-    }
-
-    fun btMulti(v: View) {
-        button13.isEnabled = true
-        if (textView.getText().toString() == "") {
-            Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
-        } else if (comprueba() == false) {
-            num1 = textView.getText().toString().toDouble()
-            operator = "x"
-            textView.text = ""
+        bt2.setOnClickListener() {
+            textView.text = textView.text.toString() + 2.toString()
         }
-    }
-
-    fun btDivi(v: View) {
-        button13.isEnabled = true
-        if (textView.getText().toString() == "") {
-            Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
-        } else if (comprueba() == false) {
-            num1 = textView.getText().toString().toDouble()
-            operator = "/"
-            textView.text = ""
+        bt3.setOnClickListener() {
+            textView.text = textView.text.toString() + 3.toString()
         }
-    }
-
-    fun btDel(v: View) {
-        button13.isEnabled = true
-        if (textView.getText().toString() == "") {
-            Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
-        } else if (textView.getText().toString() == "") {
-            Toast.makeText(context,"No se puede borrar si no hay nada", Toast.LENGTH_LONG).show()
-        } else {
-            borra()
+        bt4.setOnClickListener() {
+            textView.text = textView.text.toString() + 4.toString()
         }
-    }
+        bt5.setOnClickListener() {
+            textView.text = textView.text.toString() + 5.toString()
+        }
+        bt6.setOnClickListener() {
+            textView.text = textView.text.toString() + 6.toString()
+        }
+        bt7.setOnClickListener() {
+            textView.text = textView.text.toString() + 7.toString()
+        }
+        bt8.setOnClickListener() {
+            textView.text = textView.text.toString() + 8.toString()
+        }
+        bt9.setOnClickListener() {
+            textView.text = textView.text.toString() + 9.toString()
+        }
+        button_punto.setOnClickListener(){
+            var entrada = textView.getText().toString()
+            if (entrada == "") {
+                Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
+            } else if (entrada.contains('.')) {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                button_punto.isEnabled = false
+            } else {
+                textView.text = textView.text.toString() + "."
+            }
+        }
+        buttonDel.setOnClickListener() {
+            button_punto.isEnabled = true
+            if (textView.getText().toString() == "") {
+                Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
+            } else if (textView.getText().toString() == "") {
+                Toast.makeText(context, "No se puede borrar si no hay nada", Toast.LENGTH_LONG).show()
+            } else {
+                borra()
+            }
+        }
+        button_sumar.setOnClickListener() {
+            button_punto.isEnabled = true
+            if (textView.getText().toString() == "") {
+                Toast.makeText(context, "No has puesto nada", Toast.LENGTH_LONG).show()
+            } else {
+                num1 = textView.getText().toString().toDouble()
+                operator = "+"
+                textView.text = ""
+            }
+        }
 
-    fun btEquals(v: View) {
-        if (textView.getText().toString() == "") {
-            textView.text = num1.toString()
-        } else if (comprueba() == false) {
-            num2 = textView.getText().toString().toDouble()
-            if (operator == "+") {
+        button_restas.setOnClickListener() {
+            button_punto.isEnabled = true
+            if (textView.getText().toString() == "") {
+                textView.text = "-"
+            } else if (comprueba() == false) {
+                num1 = textView.getText().toString().toDouble()
+                operator = "-"
+                textView.text = ""
+            }
+        }
+        button_multi.setOnClickListener() {
+            button_punto.isEnabled = true
+            if (textView.getText().toString() == "") {
+                Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
+            } else if (comprueba() == false) {
+                num1 = textView.getText().toString().toDouble()
+                operator = "x"
+                textView.text = ""
+            }
+        }
+        button_div.setOnClickListener() {
+            button_punto.isEnabled = true
+            if (textView.getText().toString() == "") {
+                Toast.makeText(context, "No has puesto nada", Toast.LENGTH_SHORT).show()
+            } else if (comprueba() == false) {
+                num1 = textView.getText().toString().toDouble()
+                operator = "/"
+                textView.text = ""
+            }
+        }
+        button_resultado.setOnClickListener() {
+            if (textView.getText().toString() == "") {
+                textView.text = num1.toString()
+            } else if (comprueba() == false) {
+                num2 = textView.getText().toString().toDouble()
+                if (operator == "+") {
 
                     textView.text = sincero(sum(num1, num2)).toString()
 
-            }
-            if (operator == "-") {
+                }
+                if (operator == "-") {
                     textView.text = sincero(rest(num1, num2)).toString()
-            }
-            if (operator == "x") {
+                }
+                if (operator == "x") {
                     textView.text = sincero(multi(num1, num2)).toString()
-            }
-            if (operator == "/") {
-                if (num2 == 0.0) {
-                    Toast.makeText(context,"No se puede dividir entre 0", Toast.LENGTH_LONG).show()
-                } else {
+                }
+                if (operator == "/") {
+                    if (num2 == 0.0) {
+                        Toast.makeText(context, "No se puede dividir entre 0", Toast.LENGTH_LONG).show()
+                    } else {
 
                         textView.text = sincero(divi(num1, num2)).toString()
 
+                    }
                 }
             }
         }
     }
+            fun sum(a: Double, b: Double): Double = a + b
+            fun rest(a: Double, b: Double): Double = a - b
+            fun multi(a: Double, b: Double): Double = a * b
+            fun divi(a: Double, b: Double): Double = a / b
+            fun sincero(resultado: Double): String? {
+                var retorno = ""
+                retorno = resultado.toString()
+                if (resultado % 1.0 == 0.0) {
+                    retorno = retorno.substring(0, retorno.length - 2)
+                }
+                return retorno
+            }
 
-    fun sum(a: Double, b: Double): Double = a + b
-    fun rest(a: Double, b: Double): Double = a - b
-    fun multi(a: Double, b: Double): Double = a * b
-    fun divi(a: Double, b: Double): Double = a / b
-    fun sincero(resultado: Double): String? {
-        var retorno = ""
-        retorno = resultado.toString()
-        if (resultado % 1.0 == 0.0) {
-            retorno = retorno.substring(0, retorno.length - 2)
+            fun comprueba(): Boolean? {
+                var retorno = false
+                var c = textView.getText().toString()
+                if (c == "-" || c == "-." || c == ".-" || c == ".") {
+                    retorno = true
+                }
+                return retorno
+            }
+
+            fun borra() {
+                val Vc = ""
+                textView.text = Vc
+                num1 = 0.0
+                num2 = 0.0
+            }
         }
-        return retorno
-    }
-
-    fun comprueba(): Boolean? {
-        var retorno = false
-        var c = textView.getText().toString()
-        if (c == "-" || c == "-." || c == ".-" || c == ".") {
-            retorno = true
-        }
-        return retorno
-    }
-
-    fun borra() {
-        val Vc = ""
-        textView.text = Vc
-        num1 = 0.0
-        num2 = 0.0
-    }
-}
