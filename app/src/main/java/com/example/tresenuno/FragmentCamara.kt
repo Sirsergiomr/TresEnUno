466a6dc5cc528aceb05b5aaed91a8d889f4e8b61
Sirsergiomr <sirsergio.mr@gmail.com>
2020-11-27 18:44:27 +0100

    Terminado

--- app/src/main/java/com/example/tresenuno/FragmentCamara.kt
+++ app/src/main/java/com/example/tresenuno/FragmentCamara.kt
@@ -1,12 +1,17 @@
 package com.example.tresenuno
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Camera
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_camara.*
+import 	android.content.pm.PackageManager.*
 
 class FragmentCamara : Fragment() {
 
@@ -17,11 +22,19 @@ class FragmentCamara : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camara, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttoncamera.setOnClickListener(){
+                val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(i)
+        }
     }
 
 }
